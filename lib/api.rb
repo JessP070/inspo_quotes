99025c8bb82203d13037d7aa7efa18490481cc35
JessP070 <jcpereira070@gmail.com>
2020-07-11 17:17:14 -0700

    added notes

--- lib/api.rb
+++ lib/api.rb
@@ -1,9 +1,12 @@
 require 'httparty'
+require 'pry'
 
 class QuotesAPI
 
     def get_a_quote
      url = "https://quote-garden.herokuapp.com/api/v2/quotes/random"
     quote = HTTParty.get(url)
+    Quotes.new(quote["quote"]["quoteText"], quote["quote"]["quoteAuthor"])
+binding.pry
     end
 end
