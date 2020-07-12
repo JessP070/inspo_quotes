96160a1ef73b91aae9f3d60e22630fa326726b45
JessP070 <jcpereira070@gmail.com>
2020-07-11 18:19:10 -0700

    Created a call and create method

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,8 +2,26 @@ class CLI
     attr_accessor :api
    
    def initialize
-       @api = API.new
+    @user_input = user_input
    end
+
+   def call
+    # while(true)
+        @api = QuotesAPI.new
+        puts "             Welcome              ".yellow
+        puts "Need a daily dose of inspiration?".yellow
+        puts ""
+    end
+
+    def start
+        puts "Please type either 'y' for yes, or 'n' for no."
+        user_input = gets.chomp
+    if user_input == 'y'
+    elsif user_input == 'n'
+        puts "Aww dang it. We'll catch ya later."
+    else
+        puts "invalid input".red
+    end
    
 
 
