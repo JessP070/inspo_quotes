3cb22d6351084a13f9b2a91308deb256bb7d6cca
JessP070 <jcpereira070@gmail.com>
2020-07-11 17:20:43 -0700

    added to attr access and initialized quote author

--- lib/inspo_quotes.rb
+++ lib/inspo_quotes.rb
@@ -1,9 +1,10 @@
 class Quotes
-  attr_accessor :quoteText
+  attr_accessor :quoteText, :quoteAuthor
   @@all = [] #this is a class variable
 
   def initialize(quoteText)
       @quoteText = quoteText
+      @quoteAuthor = quoteAuthor
       @@all << self
 
   end
@@ -11,6 +12,14 @@ class Quotes
   def self.all
        @@all
        #class methods start with self
+  end
+
+  def self.quote_text
+    @quoteText
+    end
+
+  def self.quote_author
+  @quoteAuthor
   end
 
 
