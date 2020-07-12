f14c983577790c0a40ca278ae73274d98a10b944
JessP070 <jcpereira070@gmail.com>
2020-07-11 19:05:27 -0700

    made changes to ask if another quote is wanted and if not exit

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,28 +1,31 @@
 require 'colorize'
  require 'pry'
 class CLI
-    attr_accessor :api, :user_input
-   
-   def initialize
-    @user_input = user_input
-   end
-
-   def print_quote
-    @api.get_a_quote
-        Quotes.all.select do |index|
-        puts "Quote: \"#{index.quoteText}\"
-        \nAuthor: #{index.quoteAuthor}".green
-        end
-        Quotes.all.clear
+     attr_accessor :api, :user_input
+    
+    def initialize
+        @user_input = user_input
+    end
+    
+    def print_quote
+        @api.get_a_quote
+            Quotes.all.select do |index|
+            puts "Quote: \"#{index.quoteText}\"
+            \nAuthor: #{index.quoteAuthor}".green
+            end
+            Quotes.all.clear
     end
 
-   def call
-        @api = QuotesAPI.new
-        puts "             Welcome              ".yellow
-        puts "Need a daily dose of inspiration?".yellow
-        puts ""
-        start
+    def call
+        # while(true)
+            @api = QuotesAPI.new
+            puts "             Welcome              ".yellow
+            puts "Need a daily dose of inspiration?".yellow
+            puts ""
+            # reprint_quote
+            start
     end
+
 
     def start
         puts "Please type either 'y' for yes, or 'n' for no."
@@ -32,28 +35,28 @@ class CLI
 
             print_quote
             reprint_quote
-    elsif user_input == 'n'
-        puts "Aww dang it. We'll catch ya later."
-    else
-        puts "invalid input".red
-        start
+        elsif user_input == 'n'
+            puts "Aww dang it. We'll catch ya later."
+        
+        else
+            puts "invalid input".red
+            start
+        end
+         puts ""
     end
-    puts ""
-    end
-    def reprint_quote
+        def reprint_quote
 
         puts ""
           puts "Would you like another quote? press 'y' to continue press any key to exit"
           puts ""
-          user_input = gets.strip.to_s.downcase
-          if user_input == "y"
-              print_quote
-              reprint_quote
-          elsif user_input == "n"
-          else 
-          
-            puts "Thank you, hope to see you soon!"
-          end 
-      end
-  end
-   
+        user_input = gets.strip.to_s.downcase
+        if user_input == "y"
+            print_quote
+            reprint_quote
+        elsif user_input == "n"
+        else 
+        
+          puts "Thank you, hope to see you soon!"
+        end 
+    end
+end
