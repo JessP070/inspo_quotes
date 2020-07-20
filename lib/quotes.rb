417fa15c622cfb926084762b2db98731a592cd5a
JessP070 <jcpereira070@gmail.com>
2020-07-20 13:44:20 -0700

    added new sort method

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -1,10 +1,10 @@
 class Quotes
-  attr_accessor :quoteText, :quoteAuthor
+  attr_accessor :quote_text, :quote_author
   @@all = [] 
 
-  def initialize(quoteText, quoteAuthor)
-      @quoteText = quoteText
-      @quoteAuthor = quoteAuthor
+  def initialize(quote_text, quote_author)
+      @quote_text = quote_text
+      @quote_author = quote_author
       @@all << self
 
   end
@@ -14,12 +14,27 @@ class Quotes
   end
 
   def self.quote_text
-          @quoteText
+          @quote_text
   end
 
   def self.quote_author
-      @quoteAuthor
+      @quote_author
+  end
+
+  def self.sort_quote
+    @@all.sort {|a, b| a.quote_text <=> b.quote_text}
   end
 
 end
+
+#'add a feature 
+
+# think of it like a history button 
+
+# You'll ask the user if they want to see a history of all the quotes that have shown up for them 
+
+# if they say yes, you'll display all the quotes sorted by quoteText Alphab. You're not allowed to used the sort_by method but you're allowed to use .sort ( no Stack Overflow either I'll know)
+#
+# display all the quote and authors as usual (if they have authors)
+
 
