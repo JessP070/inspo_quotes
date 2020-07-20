2209375310932d82ad9b62216ddbfcaab8b1c9b2
JessP070 <jcpereira070@gmail.com>
2020-07-20 13:42:23 -0700

    added new print_sorted method

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,17 +4,27 @@ class CLI
      attr_accessor :api, :user_input
     
     def initialize
-        @user_input = user_input
+         @user_input = user_input
+      
+    end
+
+    def print_sorted
+        Quotes.sort_quote.each do |quote|
+            puts "Quote: \"#{quote.quote_text}".green
+            puts ""
+            puts "Author: \"#{quote.quote_author}".green
+            puts ""
+            
+        end
     end
     
     def print_quote
         @api.get_a_quote
-            Quotes.all.select do |index|
-            puts "Quote: \"#{index.quoteText}\"
-            \nAuthor: #{index.quoteAuthor}".green
-            end
-            Quotes.all.clear
-    end
+            puts "Quote: \"#{Quotes.all[-1].quote_text}\"
+             \nAuthor: #{Quotes.all[-1].quote_author}".green
+          
+     end
+        
 
     def call
             @api = QuotesAPI.new
@@ -24,37 +34,48 @@ class CLI
             start
     end
 
-
     def start
-        puts "Please type either 'y' for yes, or 'n' for no."
-        user_input = gets.chomp.to_s.downcase
-    if user_input == 'y'
-        puts ""
+            puts "Please type either 'y' for yes, or 'n' for no."
+            user_input = gets.chomp.to_s.downcase
+        if user_input == 'y'
+            puts ""
 
             print_quote
             reprint_quote
-        elsif user_input == 'n'
-            puts "Aww dang it. We'll catch ya later.".light_blue
+            
+         elsif user_input == 'n'
+           puts "Aww dang it. We'll catch ya later.".light_blue
         
-        else
-            puts "invalid input".red
-            start
+         else
+           puts "invalid input".red
+           start
         end
-         puts ""
-    end
-        def reprint_quote
+            puts ""
+        end
 
-        puts ""
-          puts "Would you like another quote? press 'y' to continue press any key and enter to exit"
+
+    def reprint_quote
           puts ""
-        user_input = gets.strip.to_s.downcase
+          puts "Type 'y' to generate a new quote, 'h' to see your quote history, or any key and 'enter' to exit."
+          puts ""
+          user_input = gets.strip.to_s.downcase
         if user_input == "y"
+            puts ""
             print_quote
             reprint_quote
-        elsif user_input == "n"
-        else 
-        
-          puts "Thank you, hope to see you soon!"
+            elsif user_input == "h"
+                puts ""
+                print_sorted
+                reprint_quote
+                
+            elsif user_input == "n"
+            
+            else 
+                puts""
+          puts "Thank you, hope to see you soon!".yellow
         end 
     end
+       
+    
 end
+
